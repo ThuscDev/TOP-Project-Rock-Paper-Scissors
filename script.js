d3d696b595708fb91cc20cc659f545f76741fb02
ThuscDev <tomhuscroft19@gmail.com>
2022-09-25 21:01:35 +0100

    Created prompt for player choice and created game() function but currently treats as one round

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const rpsArray = ["rock", "paper", "scissors"]
 function getComputerChoice() {
     const computerChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)]
     return computerChoice;
-    
+
 }
 
 
@@ -12,30 +12,39 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return ("It's a draw!");
-      } else if ((playerSelection.toLowerCase() === "rock") && (computerSelection.toLowerCase() === "scissors")) {
+    } else if ((playerSelection.toLowerCase() === "rock") && (computerSelection.toLowerCase() === "scissors")) {
         return ("You win! Rock beats scissors");
-      } else if (playerSelection === "rock" && computerSelection === "paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return ("You lose! Paper beats rock");
-      } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         return ("You win! Paper beats rock");
-      } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return ("You lose! Scissors beat paper");
-      } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return ("You win! Scissors beat paper");
-      } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return ("You lose!Rock beats scissors");
-      }
-}
-
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
-/*
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-*/
-
-function game() {
-    function playRound(playerSelection, computerSelection) {
-        return playRound;
     }
 }
+
+let playerSelection = prompt("Rock Paper Scissors GO: ");
+let computerSelection = getComputerChoice();
+
+// Function to play the game up to 5 rounds
+function game() {
+    for (let i = 0; i < 5; i++) {
+        if (i < 5) {
+
+            playRound();
+            console.log(i);
+        } else {
+            console.log("Game Over")
+        }
+    }
+
+    return playRound(playerSelection, computerSelection);
+}
+
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(game());
